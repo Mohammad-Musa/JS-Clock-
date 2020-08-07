9143e0b3ea69fa0edbd2513c2e8f4d5240d20aa8
HarunAltun <altunhr@gmail.com>
2020-08-07 23:53:00 +0300

    Created clock function
    Closes #2

--- scripts/handlers/starter.js
+++ scripts/handlers/starter.js
@@ -1,19 +1,7 @@
 'use strict';
 console.log('--- loading handler: _');
+const timeEl= document.querySelector('.time');
 
-const _ = (event) => {
 
-};
 
-/* handlers define user interactions
-
-  they read user input
-    from events or from the DOM
-
-  they process user data using program logic functions
-
-  they communicate results to the user
-
-  they log any important information for developers
-
-*/
+console.log('--- finished handler: _');
