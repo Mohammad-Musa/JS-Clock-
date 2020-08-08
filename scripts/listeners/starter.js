854f4a867f638f8db5c3196fcdafa735af05084c
Mohammad Musa <mhammad_musa@yahoo.com>
2020-08-08 17:57:56 +0200

    Adding listener to change the background image

--- scripts/listeners/starter.js
+++ scripts/listeners/starter.js
@@ -1,5 +1,15 @@
 'use strict';
 console.log('--- loading listener: _');
 
-document.getElementById('_')
-  .addEventListener('_', _);
+// document.getElementById('_')
+//   .addEventListener('_', _);
+
+beBtn.addEventListener('click', beBgImg);
+
+etBtn.addEventListener('click', etBgImg);
+
+idBtn.addEventListener('click', idBgImg);
+
+joBtn.addEventListener('click', joBgImg);
+
+trBtn.addEventListener('click', trBgImg);
