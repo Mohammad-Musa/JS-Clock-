c3af17464cfb8084f0549eeecaf5e13a0de18a12
Mohammad Musa <mhammad_musa@yahoo.com>
2020-08-08 20:24:38 +0200

    Changing H1 and H2 texts.

--- scripts/handlers/starter.js
+++ scripts/handlers/starter.js
@@ -13,27 +13,49 @@ const idBtn = document.querySelector('#id-btn');
 const joBtn = document.querySelector('#jo-btn');
 const trBtn = document.querySelector('#tr-btn');
 
+const changeH1 = document.querySelector('.countryName');
+const changeH2 = document.querySelector('.placeName');
+
+
+
+// Belgium
 function beBgImg(){
     document.body.style.backgroundImage = "url('/images/BE_marius_badstuber_unsplash.jpg')";   
+    changeH1.innerHTML='BELGIUM';
+    changeH2.innerHTML='Grote Markt, Brussels';
 };
 
 
+// Ethiopia
 function etBgImg(){
-    document.body.style.backgroundImage = "url('/images/ET_mulugeta_wolde_unsplash.jpg')"; 
+    document.body.style.backgroundImage = "url('/images/ET_mulugeta_wolde_unsplash.jpg')";
+    changeH1.innerHTML='ETHIOPIA';
+    changeH2.innerHTML='Addis Ababa'; 
 };
 
 
+
+
+
+// Indonesia
 function idBgImg(){
     document.body.style.backgroundImage = "url('/images/ID_rashel_ochoa_unsplash.jpg')";
+    changeH1.innerHTML='INDONESIA';
+    changeH2.innerHTML='Jakarta'; 
 };
 
-
+// Jordan
 function joBgImg(){
     document.body.style.backgroundImage = "url('/images/JO_brian_kairuz_unsplash.jpg')";
+    changeH1.innerHTML='JORDAN';
+    changeH2.innerHTML='Petra'; 
 };
 
+// Turkey
 function trBgImg(){
     document.body.style.backgroundImage = "url('/images/TR_yonatan_anugerah_unsplash.jpg')";
+    changeH1.innerHTML='TURKEY';
+    changeH2.innerHTML='Ankara'; 
 };
 
 
