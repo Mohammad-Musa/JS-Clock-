ef989f6208ea96df0c4b682f45b187212cc67b49
Yared <yayeyaye6@gmail.com>
2020-08-09 00:39:15 +0200

    updating images
    saving the images

--- scripts/handlers/starter.js
+++ scripts/handlers/starter.js
@@ -1,19 +1,65 @@
 'use strict';
 console.log('--- loading handler: _');
 
-const _ = (event) => {
+const timeEl= document.querySelector('.time');
+
+
+// change the background image handler
+
+
+const beBtn = document.querySelector('#be-btn');
+const etBtn = document.querySelector('#et-btn');
+const idBtn = document.querySelector('#id-btn');
+const joBtn = document.querySelector('#jo-btn');
+const trBtn = document.querySelector('#tr-btn');
+
+const changeH1 = document.querySelector('.countryName');
+const changeH2 = document.querySelector('.placeName');
+
+
+
+// Belgium
+function beBgImg(){
+
+    document.body.style.backgroundImage = "url('/images/BE_fotografierende_unsplash.jpg')";   
+    changeH1.innerHTML='BELGIUM';
+    changeH2.innerHTML='Grote Markt, Brussels'; 
 
 };
 
-/* handlers define user interactions
 
-  they read user input
-    from events or from the DOM
+// Ethiopia
+function etBgImg(){
 
-  they process user data using program logic functions
+    document.body.style.backgroundImage = "url('/images/ET_mulugeta_wolde_unsplash.jpg')";
+    changeH1.innerHTML='ETHIOPIA';
+    changeH2.innerHTML='Addis Ababa'; 
 
-  they communicate results to the user
+};
 
-  they log any important information for developers
 
-*/
+
+// Indonesia
+function idBgImg(){
+    document.body.style.backgroundImage = "url('/images/ID_radoslav_unsplash.jpg')";
+    changeH1.innerHTML='INDONESIA';
+    changeH2.innerHTML='Jakarta'; 
+};
+
+// Jordan
+function joBgImg(){
+    document.body.style.backgroundImage = "url('/images/JO_alessa_unsplash.jpg')";
+    changeH1.innerHTML='JORDAN';
+    changeH2.innerHTML='Petra'; 
+};
+
+
+// Turkey
+function trBgImg(){
+    document.body.style.backgroundImage = "url('/images/TR_alex_azabache_unsplash.jpg')";
+    changeH1.innerHTML='TURKEY';
+    changeH2.innerHTML='Ankara'; 
+};
+
+
+console.log('--- finished handler: _');
